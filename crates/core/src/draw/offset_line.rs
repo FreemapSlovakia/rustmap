affffbe0c3f20b41aa1df24f8f135032e8287798
Martin Ždila <m.zdila@gmail.com>
2025-12-15 18:53:40 +0100

    lowered accuracy for possible performance

--- crates/core/src/draw/offset_line.rs
+++ crates/core/src/draw/offset_line.rs
@@ -2,6 +2,8 @@ use cavalier_contours::polyline::{PlineSource, PlineSourceMut, PlineVertex, Poly
 use geo::{Coord, LineString};
 
 pub fn offset_line_string(line_string: &LineString, offset: f64) -> LineString {
+    let _span = tracy_client::span!("offset_line::offset_line_string");
+
     let mut result = Vec::<Coord>::new();
 
     let mut polyline = Polyline::new();
@@ -11,7 +13,7 @@ pub fn offset_line_string(line_string: &LineString, offset: f64) -> LineString {
     }
 
     for pc in polyline.parallel_offset(offset) {
-        for v in pc.arcs_to_approx_lines(1.0).unwrap().vertex_data {
+        for v in pc.arcs_to_approx_lines(3.0).unwrap().vertex_data {
             result.push(Coord { x: v.x, y: v.y });
         }
     }
