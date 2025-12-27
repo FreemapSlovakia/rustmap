4abf07fc33cedcb93c556957760417af0e04e776
Martin Ždila <m.zdila@gmail.com>
2025-12-27 21:09:06 +0100

    added rendering grave_yard (fixes #9)

--- rust/crates/core/src/layers/landuse.rs
+++ rust/crates/core/src/layers/landuse.rs
@@ -34,7 +34,7 @@ pub fn render(ctx: &Ctx, client: &mut Client, svg_cache: &mut SvgCache) -> Layer
                     ELSE type
                 END AS type,
                 ST_Intersection(ST_MakeValid(geometry), ST_Expand(ST_MakeEnvelope($1, $2, $3, $4, 3857), 100)) AS geometry,
-                position(type || ',' IN 'pedestrian,footway,pitch,library,baracks,parking,cemetery,place_of_worship,dam,weir,clearcut,wetland,scrub,orchard,vineyard,railway,landfill,scree,blockfield,quarry,park,garden,allotments,village_green,grass,recreation_ground,fell,bare_rock,heath,meadow,wood,forest,golf_course,grassland,farm,zoo,farmyard,hospital,kindergarten,school,college,university,retail,commercial,industrial,residential,farmland,glacier,') AS z_order
+                position(type || ',' IN 'pedestrian,footway,pitch,library,baracks,parking,cemetery,grave_yard,place_of_worship,dam,weir,clearcut,wetland,scrub,orchard,vineyard,railway,landfill,scree,blockfield,quarry,park,garden,allotments,village_green,grass,recreation_ground,fell,bare_rock,heath,meadow,wood,forest,golf_course,grassland,farm,zoo,farmyard,hospital,kindergarten,school,college,university,retail,commercial,industrial,residential,farmland,glacier,') AS z_order
             FROM osm_landusages{}
             WHERE
                 {excl_types}
@@ -96,7 +96,7 @@ pub fn render(ctx: &Ctx, client: &mut Client, svg_cache: &mut SvgCache) -> Layer
             "allotments" => {
                 colour_area(colors::ALLOTMENTS)?;
             }
-            "cemetery" => {
+            "cemetery" | "grave_yard" => {
                 colour_area(colors::GRASSY)?;
                 pattern_area("grave.svg")?;
             }
